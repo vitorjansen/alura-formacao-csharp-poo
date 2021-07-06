2a41da2d246d8e05927b0fca13b560505948ad1b
Vitor Jansen <vitorvieira.id@gmail.com>
2021-07-06 16:31:07 -0300

    Módulo#1 - feat: Parte 04 - Variáveis de texto

---
+++ 01-primeiros-passos/M01-PrimeiroPassos/A03-VariaveisDeTexto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace A03_VariaveisDeTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letra = 'a';
+            Console.WriteLine(letra);
+
+            char valor = (char)65;
+            Console.WriteLine(valor);
+
+            valor = (char)(valor + 1);
+            Console.WriteLine(valor);
+
+            string palavra = "alura cursos online de tecnologia";
+            Console.WriteLine(palavra);
+
+            palavra = palavra + 2020;
+            Console.WriteLine(palavra);
+        }
+    }
+}
