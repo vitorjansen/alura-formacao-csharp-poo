2a90ce7321d8ee26161aa8f0174d05683ad1076f
Vitor Jansen <vitorvieira.id@gmail.com>
2021-07-06 17:35:04 -0300

    Módulo#1 - feat: Parte 06 - Laço de repetição - While

---
+++ 01-primeiros-passos/M01-PrimeiroPassos/A05-LacosDeRepeticao/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace A05_LacosDeRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Calcula poupança");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            while (contadorMes <= 12)
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$" + valorInvestido);
+
+                contadorMes += 1;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
