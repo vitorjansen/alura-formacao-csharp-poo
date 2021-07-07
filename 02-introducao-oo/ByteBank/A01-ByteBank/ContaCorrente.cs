823ddc3a03edce619c3e95245d880099e2be5251
Vitor Jansen <vitorvieira.id@gmail.com>
2021-07-06 22:42:39 -0300

    Módulo#2 - feat: Parte 01 - A primeira classe

---
+++ 02-introducao-oo/ByteBank/A01-ByteBank/ContaCorrente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A01_ByteBank
+{
+    class ContaCorrente
+    {
+        public string titular;
+        public int agencia;
+        public int numero;
+        public double saldo;
+    }
+}
---
+++ 02-introducao-oo/ByteBank/A01-ByteBank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A01_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ContaCorrente();
+        }
+    }
+}
