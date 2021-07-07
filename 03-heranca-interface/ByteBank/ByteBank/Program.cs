053aecf601021aa25aa56476332a5a2bc7611f7a
Vitor Jansen <vitorvieira.id@gmail.com>
2021-07-07 13:58:41 -0300

    Módulo#3 - feat: Parte 01 - Trabalhando com sobrecarga

---
+++ 03-heranca-interface/ByteBank/ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using ByteBank.Funcionarios;
+using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario carlos = new Funcionario();
+            carlos.Nome = "Carlos";
+            carlos.Cpf = "546.879.157-20";
+            carlos.Salario = 2000;
+
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.Cpf = "454.658.148-3";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(carlos);
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
+        }
+    }
+}
