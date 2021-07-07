a6e7dabcc6984410de5ce8ac28899b5a99168a45
Vitor Jansen <vitorvieira.id@gmail.com>
2021-07-06 23:48:10 -0300

    Módulo#2 - feat: Parte 03 - Comportamentos da classe e implementação dos métodos

--- 02-introducao-oo/ByteBank/A01-ByteBank/ContaCorrente.cs
+++ 02-introducao-oo/ByteBank/A01-ByteBank/ContaCorrente.cs
@@ -12,5 +12,34 @@ namespace A01_ByteBank
         public int agencia;
         public int numero;
         public double saldo;
+
+        //dentro da class ContaCorrente
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+
+            this.saldo -= valor;
+            return true;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
     }
 }
