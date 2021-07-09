a3037b872c1b53a5f417c79c1a287cdbdeaa7937
Vitor Jansen <vitorvieira.id@gmail.com>
2021-07-09 19:59:03 -0300

    Módulo#5 - feat: Parte 05 - Gerenciando pacotes com o NuGet

--- 05-bibliotecas-dlls-docs-nuget/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ 05-bibliotecas-dlls-docs-nuget/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ByteBank.Modelos.Funcionarios;
+using Humanizer;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -12,11 +13,17 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(374, 3627362);
-            conta.Sacar(213);
-            FuncionarioAutenticavel carlos = null;
-            carlos.Autenticar("key");
 
+
+            // Dia 17 de Agosto de 2018
+            DateTime dataFimPagamento = new DateTime(2021, 8, 17);
+            // Data corrente no momento de execução do código
+            DateTime dataCorrente = DateTime.Now;
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
 
             Console.ReadLine();
         }
