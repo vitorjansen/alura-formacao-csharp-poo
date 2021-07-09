ed0de341d58f6b9856caae7d5d2e368b648d8dce
Vitor Jansen <vitorvieira.id@gmail.com>
2021-07-09 15:27:58 -0300

    Módulo#4 - feat: Parte 06 - Bloco finally versus using

--- 04-execoes/ByteBank/Program.cs
+++ 04-execoes/ByteBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,37 +13,45 @@ namespace ByteBank
         {
             try
             {
-                ContaCorrente conta1 = new ContaCorrente(4564, 789684);
-                ContaCorrente conta2 = new ContaCorrente(7891, 456794);
-
-                conta1.Transferir(10000, conta2);
-                conta1.Sacar(10000);
+                CarregarContas();
             }
-            catch (OperacaoFinanceiraException e)
+            catch (Exception)
             {
-                Console.WriteLine(e.Message);
-                Console.WriteLine(e.StackTrace);
-
-                Console.WriteLine("Informações da INNER EXCEPTION (exceção interna):");
-
-                Console.WriteLine(e.InnerException.Message);
-                Console.WriteLine(e.InnerException.StackTrace);
-            }
-            catch (SaldoInsuficienteException ex)
-            {
-                Console.WriteLine(ex.Message);
-                Console.WriteLine("Exceção do tipo SaldoInsuficienteException");
-            }
-            catch (ArgumentException e)
-            {
-                Console.WriteLine(e.Message);
-                Console.WriteLine(e.ParamName);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("CATCH NO METODO MAIN");
             }
         }
 
+        private static void CarregarContas()
+        {
+            //Método com using
+            using (LeitorDeArquivo leitor = new LeitorDeArquivo("contas.txt"))
+            {
+                leitor.LerProximaLinha();
+                leitor.LerProximaLinha();
+                leitor.LerProximaLinha();
+            }
+
+            //Método com try/catch/finally
+
+            //LeitorDeArquivo leitor = null;
+            //try
+            //{
+            //    leitor = new LeitorDeArquivo("contas.txt");
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //}
+            //catch (IOException e)
+            //{
+            //    Console.WriteLine("Exceção do tipo IOException capturada e tratada.");
+            //}
+            //finally
+            //{
+            //    if (leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+            //}
+        }
     }
 }
