6eeecb041b3885353c7cdec73f023e6bc8708d2b
Vitor Jansen <vitorvieira.id@gmail.com>
2021-07-09 16:51:47 -0300

    Módulo#5 - feat: Parte 01 - Introdução a bibliotecas e adição de referências

---
+++ 05-bibliotecas-dlls-docs-nuget/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ByteBank.Modelos;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(374, 3627362);
+            Console.WriteLine(conta.Numero);
+
+
+            Console.ReadLine();
+        }
+    }
+}
