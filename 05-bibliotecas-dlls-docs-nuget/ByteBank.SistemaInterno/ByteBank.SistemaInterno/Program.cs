3e8645b0e6e26c05aab47f449ac67940851f7622
Vitor Jansen <vitorvieira.id@gmail.com>
2021-07-09 17:59:28 -0300

    Módulo#5 - feat: Parte 03 - Referenciando dlls

---
+++ 05-bibliotecas-dlls-docs-nuget/ByteBank.SistemaInterno/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(342, 327323);
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
