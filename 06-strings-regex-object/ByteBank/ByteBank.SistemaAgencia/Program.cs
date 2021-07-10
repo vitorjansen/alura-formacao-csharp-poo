e0dadbafe578bd9bf45779d9aa225fac41c801fa
Vitor Jansen <vitorvieira.id@gmail.com>
2021-07-09 21:55:54 -0300

    Módulo#6 - feat: Parte 01 - Substrings

---
+++ 06-strings-regex-object/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ByteBank.Modelos;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos.Funcionarios;
+using Humanizer;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string url = "pagina?argumentos";
+            string argumentos = url.Substring(7);
+            Console.WriteLine(argumentos);
+
+            Console.ReadLine();
+        }
+    }
+}
