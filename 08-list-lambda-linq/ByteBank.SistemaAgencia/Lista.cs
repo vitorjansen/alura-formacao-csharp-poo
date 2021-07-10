6518e6ff89357b07bc557a2100d75c07266cf404
Vitor Jansen <vitorvieira.id@gmail.com>
2021-07-10 15:14:45 -0300

    Módulo#8 - feat: Parte 01 - Tipo List e criação de métodos de extensão

---
+++ 08-list-lambda-linq/ByteBank.SistemaAgencia/Lista.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Lista<T>
+    {
+        private T[] _itens;
+        private int _proximaPosicao;
+        public int Tamanho
+        {
+            get
+            {
+                return _proximaPosicao;
+            }
+        }
+
+        public Lista(int capacidadeInicial = 5)
+        {
+            _itens = new T[capacidadeInicial];
+            _proximaPosicao = 0;
+
+        }
+
+        public void Adicionar(T item)
+        {
+            VerificarCapacidade(_proximaPosicao + 1);
+
+
+            _itens[_proximaPosicao] = item;
+            _proximaPosicao++;
+        }
+
+        private void VerificarCapacidade(int tamanhoNecessario)
+        {
+            if (_itens.Length >= tamanhoNecessario)
+            {
+                return;
+            }
+            int novoTamanho = _itens.Length * 2;
+            Console.WriteLine("Aumentando capacidade da lista!");
+            if (novoTamanho < tamanhoNecessario)
+            {
+                novoTamanho = tamanhoNecessario;
+            }
+
+            T[] novoArray = new T[novoTamanho];
+            for (int indice = 0; indice < _itens.Length; indice++)
+            {
+                novoArray[indice] = _itens[indice];
+            }
+            _itens = novoArray;
+        }
+
+        public void Remover(T item)
+        {
+            int indiceItem = -1;
+
+            for (int i = 0; i < _proximaPosicao; i++)
+            {
+                T itemAtual = _itens[i];
+
+                if (itemAtual.Equals(item))
+                {
+                    indiceItem = i;
+                    break;
+                }
+            }
+
+            for (int i = indiceItem; i < _proximaPosicao - 1; i++)
+            {
+                _itens[i] = _itens[i + 1];
+            }
+
+            _proximaPosicao--;
+        }
+
+        public void EscreverListaNaTela()
+        {
+            for (int i = 0; i < _proximaPosicao; i++)
+            {
+                T conta = _itens[i];
+            }
+        }
+
+        public T GetItemNoIndice(int indice)
+        {
+            if (indice < 0 || indice >= _proximaPosicao)
+            {
+                throw new ArgumentOutOfRangeException(nameof(indice));
+            }
+
+            return _itens[indice];
+        }
+
+        public T this[int indice]
+        {
+            get
+            {
+                return GetItemNoIndice(indice);
+            }
+        }
+
+        public void AdicionarVarios(params T[] itens)
+        {
+            foreach (T conta in itens)
+            {
+                Adicionar(conta);
+            }
+        }
+    }
+}
---
+++ 08-list-lambda-linq/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> idades = new List<int>();
+
+            idades.AdicionarVarios(1, 5, 14, 25, 38, 61);
+
+            idades.Remove(5);
+
+            for (int i = 0; i < idades.Count; i++)
+            {
+                Console.WriteLine(idades[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
