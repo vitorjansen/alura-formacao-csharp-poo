9635ce788b61a70701c9f59eaa3676920946556c
Vitor Jansen <vitorvieira.id@gmail.com>
2021-07-10 10:36:38 -0300

    Módulo#7 - feat: Parte 01 - Arrays

---
+++ 07-array-tiposgenericos/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] idades = new int[] { 15, 28, 35, 50, 28 };
+
+            int acumulador = 0;
+            for (int indice = 0; indice < idades.Length; indice++)
+            {
+                acumulador += idades[indice];
+                Console.WriteLine($"Valor no índice {indice}: {idades[indice]}");
+
+            }
+            int media = acumulador / idades.Length;
+            Console.WriteLine(media);
+
+            Console.ReadLine();
+        }
+    }
+}
