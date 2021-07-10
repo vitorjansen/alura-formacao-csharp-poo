97fd79a7d88a5cc9360fb2314392263fbd27b3a0
Vitor Jansen <vitorvieira.id@gmail.com>
2021-07-10 20:37:46 -0300

    Módulo#9 - feat: Parte 01 - FileStream, leitura dinâmica com Read e Buffer e decodificação com Enconding UTF

---
+++ 09-streams-input-output/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,39 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        {
+            var arquivo = "contas.txt";
+
+            var fluxoDoArquivo = new FileStream(arquivo, FileMode.Open);
+            var buffer = new byte[1024];
+
+            var quantidadeBytesLidos = -1;
+            while (quantidadeBytesLidos != 0)
+            {
+                quantidadeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            var encoding = new UTF8Encoding();
+            var texto = encoding.GetString(buffer);
+
+            Console.Write(texto);
+        }
+    }
+} 
+ 
